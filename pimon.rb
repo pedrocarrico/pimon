7dc5f97fe32bbb80da9673307d1c8227a1084e76
Pedro Carriço <pedro.carrico@gmail.com>
2012-08-21 22:06:06 +0100

    Remove whitespace and unnecessary code

--- pimon.rb
+++ pimon.rb
@@ -6,18 +6,16 @@ require 'yaml'
 
 configure do
   config = YAML.load_file('config.yml')
-
+  
   if config['basic_auth']['enabled']
     use Rack::Auth::Basic, "Restricted Area" do |username, password|
       [username, password] == [config['basic_auth']['username'], config['basic_auth']['password']]
     end
   end
-
+  
   set :public_folder, 'public'
   set :redis, Redis.new(:path => config['redis']['socket'])
 end
-
-
 
 get '/' do
   @time, @cpu, @mem, @swap = settings.redis.pipelined do
@@ -26,20 +24,10 @@ get '/' do
     settings.redis.lrange(Queues::MEM,  0, -1)
     settings.redis.lrange(Queues::SWAP, 0, -1)
   end
-
+  
   @cpu  = @cpu.reduce([]){ |acc, value| acc << value.to_i}
   @mem  = @mem.reduce([]){ |acc, value| acc << value.to_i}
   @swap = @swap.reduce([]){ |acc, value| acc << value.to_i}
-
-  haml :index
-end
-
-
-private
-
-def read_config
   
-
-
-
+  haml :index
 end
