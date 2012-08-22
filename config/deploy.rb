c4ce9a858285a8300bb7e338c4d4727c3d9c4558
Pedro Carriço <pedro.carrico@gmail.com>
2012-08-22 22:14:39 +0100

    Add deploy task to check and ask for the basic_auth username and password

--- config/deploy.rb
+++ config/deploy.rb
@@ -17,6 +17,7 @@ set :use_sudo, false
 
 namespace :deploy do
   task :start, :roles => [:web, :app] do
+    deploy.check_basic_auth
     run "cd #{deploy_to}/current && nohup thin -C config/thin/config.yml -R config/config.ru start"
   end
   
@@ -26,11 +27,33 @@ namespace :deploy do
   
   task :restart, :roles => [:web, :app] do
     deploy.stop
+    deploy.check_basic_auth
     deploy.start
   end
   
   task :cold do
     deploy.update
+    deploy.check_basic_auth
     deploy.start
+  end
+  
+  desc "After deploying check if there's need for a new username/password for the basic_auth"
+  task :check_basic_auth, :roles => :app do
+    require "yaml"
+    set :username, proc { Capistrano::CLI.ui.ask("username : ") }
+    set :password, proc { Capistrano::CLI.password_prompt("password : ") }
+    set :shared_config, "#{deploy_to}/shared/production.yml"
+    
+    if 'true' ==  capture("if [ -e #{shared_config} ]; then echo 'true'; fi").strip
+      run "cp #{shared_config} #{deploy_to}/current/config/production.yml"
+    else
+      production_config = YAML::load_file('config/production.yml')
+      production_config['basic_auth']['username'] = username
+      production_config['basic_auth']['password'] = password
+      
+      put YAML::dump(production_config), shared_config, :mode => 0664
+      run "cp #{shared_config} #{deploy_to}/current/config/production.yml"
+      puts "DONE"
+    end
   end
 end
