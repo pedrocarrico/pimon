e3c90a1e57cd644e9355caa4f3b344d899a60e4c
Pedro Carriço <pedro.carrico@gmail.com>
2012-08-27 22:24:56 +0100

    Add favicon with stats information and added more configuration options

--- pimon.rb
+++ pimon.rb
@@ -20,6 +20,7 @@ class Pimon < Sinatra::Base
     
     set :public_folder, 'public'
     set :redis, Redis.new(:path => config['redis']['socket'])
+    set :config, config
   end
   
   configure :test do
@@ -34,23 +35,35 @@ class Pimon < Sinatra::Base
     end
     set :public_folder, 'public'
     set :redis, MockRedis.new
+    set :config, config
   end
   
   get '/' do
     last_update = settings.redis.lindex(Queues::TIME, 5)
-    last_modified(DateTime.parse(last_update)) if last_update
+    last_modified(DateTime.parse(last_update)) if ENV['RACK_ENV'] != 'development' && last_update
     
-    @time, @cpu, @mem, @swap = settings.redis.pipelined do
+    @o = {}
+    
+    @o[:time] = {}
+    @o[:cpu] = {}
+    @o[:mem] = {}
+    @o[:swap] = {}
+    
+    @o[:cpu][:color] = settings.config['chart']['cpu']['color']
+    @o[:mem][:color] = settings.config['chart']['mem']['color']
+    @o[:swap][:color] = settings.config['chart']['swap']['color']
+    
+    @o[:time][:stats], @o[:cpu][:stats], @o[:mem][:stats], @o[:swap][:stats] = settings.redis.pipelined do
       settings.redis.lrange(Queues::TIME, 0, -1)
       settings.redis.lrange(Queues::CPU,  0, -1)
       settings.redis.lrange(Queues::MEM,  0, -1)
       settings.redis.lrange(Queues::SWAP, 0, -1)
     end
-
-    @time = @time.reduce([]){ |acc, value| acc << (/\d\d:\d\d:\d\d/.match(value))[0]}
-    @cpu  = @cpu.reduce([]){ |acc, value| acc << value.to_i}
-    @mem  = @mem.reduce([]){ |acc, value| acc << value.to_i}
-    @swap = @swap.reduce([]){ |acc, value| acc << value.to_i}
+    
+    @o[:time][:stats] = @o[:time][:stats].reduce([]){ |acc, value| acc << (/\d\d:\d\d:\d\d/.match(value))[0]}
+    @o[:cpu][:stats]  = @o[:cpu][:stats].reduce([]){ |acc, value| acc << value.to_i}
+    @o[:mem][:stats]  = @o[:mem][:stats].reduce([]){ |acc, value| acc << value.to_i}
+    @o[:swap][:stats] = @o[:swap][:stats].reduce([]){ |acc, value| acc << value.to_i}
     
     haml :index
   end
