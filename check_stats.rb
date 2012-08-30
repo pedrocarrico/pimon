07b63a78548b17ab4e8aae5c6769472593e84448
Pedro Carriço <pedro.carrico@gmail.com>
2012-08-30 21:12:05 +0100

    Fix Bundler require

--- check_stats.rb
+++ check_stats.rb
@@ -1,6 +1,6 @@
 require 'bundler/setup'
 ENV['RACK_ENV'] = 'development' unless ENV['RACK_ENV']
-Bundler.require_env(:default, ENV['RACK_ENV'])
+Bundler.require(:default, ENV['RACK_ENV'])
 
 require './lib/pimon_config'
 require './lib/stats_collector'
