2a98f1c423d452c7b12a5167013510d9c043762d
Pedro Carriço <pedro.carrico@gmail.com>
2014-06-21 18:48:08 +0100

    Prevent division by 0 Fixes #2

--- lib/pimon/probe/system_memory.rb
+++ lib/pimon/probe/system_memory.rb
@@ -12,6 +12,7 @@ class SystemMemory
     
     memory = `free -o -m`.split(/\n/)[index].split(" ")
     
+    return 0 if memory[1].to_i == 0
     # memory[1] holds total memory
     # memory[2] holds used memory
     ((memory[2].to_f / memory[1].to_f) * 100).to_i
